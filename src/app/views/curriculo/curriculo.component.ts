981a754db734b70bb947ee5b84ee4862b8d9ef4a
ruan.souza <ruan.souza@ndd.tech>
2024-08-13 18:28:04 -0300

    home finalizada

--- src/app/views/curriculo/curriculo.component.ts
+++ src/app/views/curriculo/curriculo.component.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-curriculo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    CardComponent, 
+    MatIconModule,
+    RouterLink,
+    MatCardModule
+  ],
   templateUrl: './curriculo.component.html',
   styleUrl: './curriculo.component.scss'
 })
