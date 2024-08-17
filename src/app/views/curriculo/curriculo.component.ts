c9a7068d2d5f9bd59b53473599496266a8255bf6
ruan.souza <ruan.souza@ndd.tech>
2024-08-17 15:06:49 -0300

    atualização

--- src/app/views/curriculo/curriculo.component.ts
+++ src/app/views/curriculo/curriculo.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardComponent } from '../../shared/card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-curriculo',
@@ -13,11 +15,25 @@ import { RouterLink } from '@angular/router';
     CardComponent, 
     MatIconModule,
     RouterLink,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   templateUrl: './curriculo.component.html',
   styleUrl: './curriculo.component.scss'
 })
 export class CurriculoComponent {
+  private _snackBar = inject(MatSnackBar);
+  private message: string = 'Download iniciado...'
 
+  downloadPDF() {
+    const link = document.createElement('a');
+
+    this._snackBar.open(this.message, 'Ok', {
+      duration: 1000,
+    });
+
+    link.href = '../../assets/Curriculo_Atualizado.pdf'; // Caminho para o arquivo PDF na pasta 'assets'
+    link.download = 'Curriculo_Atualizado.pdf'; // Nome do arquivo ao ser baixado
+    link.click();
+  }
 }
